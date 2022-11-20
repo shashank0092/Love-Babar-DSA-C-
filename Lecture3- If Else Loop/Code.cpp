fde22f2cd80cb560808025764d5da71b07b4322c
SHASHANK <shashank7016204503@gmail.com>
2022-11-20 08:48:46 +0530

    Lecture 2 Code-Basic Of C++

---
+++ Lecture3- If Else Loop/Code.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    int b;
+
+    cout<<"ENTER VALUE OF TWO NUMBERS "<<endl;
+    cin>>a>>b;
+
+    if(a>b){
+        cout<<"THE MAXIMUM NUMBER IS "<<a<<endl;
+    }
+    else{
+        cout<<"THE MAXIMUM NUMBER IS "<<b<<endl;
+    }
+
+    return 0;
+}
